98e7c5f0ffecad9ae1ed25fad98792e5285533a6
Atif Ali <atif276ali@gmail.com>
2023-12-02 02:40:07 +0530

    auth added

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div>Ask Question</div>;
+}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="h1-bold">Next.js 14 is awesome</h1>
-      <h2 className="h2-bold">Next.js 14 is awesome</h2>
-    </main>
-  );
-}
