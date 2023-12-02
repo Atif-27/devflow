6ce8de56372bbaaf09d22029c7bd1760e31d0c7d
Atif Ali <atif276ali@gmail.com>
2023-12-02 12:32:57 +0530

    Added metadata and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,28 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community driven platform for asking and answering questions, get help, share knowledge and collaborate",
+  icons: {
+    icon: "/assets/images/site-logo",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +30,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable}  ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">Hello</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
